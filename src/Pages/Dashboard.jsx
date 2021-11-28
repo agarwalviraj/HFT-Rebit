4b3cb3adf60b838b4318927c3e1d10c8597a2908
vedant-11 <69393820+vedant-11@users.noreply.github.com>
2021-11-28 14:40:54 +0530

    Update Dashboard

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,7 +1,25 @@
-import React, { useState } from "react";
-import "./dashboard.css";
+import React, { useState, useEffect } from "react";
+import "../styles/dashboard.css";
 import NavBar from "../Components/NavBar";
 import { ethers } from "ethers";
+import axios from "axios";
+import plastic from "../assets/plastic.jpg";
+import metal from "../assets/metal.jpg";
+import paper from "../assets/paper.jpg";
+import glass from "../assets/glass.jpg";
+
+const startPayment = async ({ setError, setTxs, ether, addr }) => {
+  try {
+    await axios.post(process.env.REACT_APP_BACKEND_URL + "/send", {
+      address: addr,
+      amount: ether,
+    });
+
+    console.log({ ether, addr });
+  } catch (err) {
+    setError(err.message);
+  }
+};
 
 const Dashboard = () => {
   // meta mask connection code
@@ -10,6 +28,19 @@ const Dashboard = () => {
   const [defaultAccount, setDefaultAccount] = useState(null);
   const [userBalance, setUserBalance] = useState(null);
   const [connButtonText, setConnButtonText] = useState("Connect wallet");
+  const [count, setCount] = useState(0);
+  const [inputValue, setInputValue] = useState("");
+
+  const [amount, setAmount] = useState(0);
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await axios.get(process.env.REACT_APP_BACKEND_URL);
+      } catch (error) {
+        console.error("heroku error");
+      }
+    })();
+  }, []);
 
   const connectWalletHandler = () => {
     if (window.ethereum) {
@@ -26,9 +57,12 @@ const Dashboard = () => {
   const accountChangedHandler = (newAccount) => {
     setDefaultAccount(newAccount);
     getUserBalance(newAccount.toString());
+    setConnButtonText("Wallet Connected");
   };
 
   const getUserBalance = (address) => {
+    if (!window.ethereum) return;
+
     window.ethereum
       .request({ method: "eth_getBalance", params: [address, "latest"] })
       .then((balance) => {
@@ -40,9 +74,24 @@ const Dashboard = () => {
     window.location.reload();
   };
 
-  window.ethereum.on("accountsChanged", accountChangedHandler);
+  const setNewAmount = () => {
+    setAmount(count);
+  };
 
-  window.ethereum.on("chainChanegd", chainChangedHandler);
+  useEffect(() => {
+    if (!window.ethereum) return;
+
+    window.ethereum?.on("accountsChanged", accountChangedHandler);
+
+    window.ethereum?.on("chainChanegd", chainChangedHandler);
+
+    return () => {
+      if (!window.ethereum) return;
+      window.ethereum?.removeListener("accountsChanged", accountChangedHandler);
+
+      window.ethereum?.removeListener("chainChanegd", chainChangedHandler);
+    };
+  }, [window.ethereum]);
 
   return (
     <>
@@ -56,8 +105,31 @@ const Dashboard = () => {
             the smart bin. We don’t force you but reward you for the kindness
             towards nature!
           </p>
-          <input type="text" className="wallet-key" placeholder="0 ETH" />
+          <div className="claim-div">
+            <div className="input-value">{count} ETH</div>
+
+            {/* <input
+            type="text"
+            className="wallet-key"
+            placeholder={count + " ETH"}
+            onChange={(e) => setAmount(e.target.value)}
+          /> */}
+
+            <button
+              type="submit"
+              className="claim-button"
+              onClick={() =>
+                startPayment({ ether: count, addr: defaultAccount })
+              }
+            >
+              Claim
+            </button>
+          </div>
+          <p className="counter-heading">
+            Select the amount you contributed in the smart bin
+          </p>
         </div>
+
         <div className="seperator"></div>
         <div className="bank-board">
           <h3 className="balance-heading">Your Balance</h3>
@@ -70,9 +142,58 @@ const Dashboard = () => {
           <p className="address">{userBalance}</p>
 
           <button className="wallet-button" onClick={connectWalletHandler}>
-            {" "}
-            Connect your wallet
+            {connButtonText}
           </button>
+        </div>
+      </div>
+
+      <div className="counter-btn-holder">
+        <div className="counter-holder">
+          <div className="counter-container">
+            <img src={plastic} className="img-container" />
+            <button
+              className="conter-btn"
+              onClick={() => {
+                setCount(parseFloat(count) + 0.1);
+              }}
+            >
+              plastic
+            </button>
+          </div>
+
+          <div className="counter-container1">
+            <img src={metal} className="img-container" />
+            <button
+              className="conter-btn"
+              onClick={() => {
+                setCount(parseFloat(count) + 0.01);
+              }}
+            >
+              metal
+            </button>
+          </div>
+          <div className="counter-container2">
+            <img src={paper} className="img-container" />
+            <button
+              className="conter-btn"
+              onClick={() => {
+                setCount(parseFloat(count) + 0.1);
+              }}
+            >
+              paper
+            </button>
+          </div>
+          <div className="counter-container3">
+            <img src={glass} className="img-container" />
+            <button
+              className="conter-btn"
+              onClick={() => {
+                setCount(parseFloat(count) + 0.1);
+              }}
+            >
+              glass
+            </button>
+          </div>
         </div>
       </div>
     </>
